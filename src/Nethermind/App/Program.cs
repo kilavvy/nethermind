7ea4d40233a1b02070a17fdcb106808410e05f0c
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-01-08 20:01:15 +0100

    Add test project

---
+++ src/Nethermind/App/Program.cs
@@ -0,0 +1,11 @@
+﻿using Nethermind.Evm;
+using Nethermind.State;
+
+EvmState state = EvmState.RentTopLevel(
+    10_000,
+    ExecutionType.CALL,
+    Snapshot.Empty,
+    new(),
+    new());
+
+Console.WriteLine(state.GasAvailable);
