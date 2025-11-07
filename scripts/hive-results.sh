deab5c910023222cd7723ea694cf165be597564a
Ruben Buniatyan <rubo@users.noreply.github.com>
2025-11-07 19:49:54 +0100

    test

--- scripts/hive-results.sh
+++ scripts/hive-results.sh
@@ -35,29 +35,30 @@ for passed in "true" "false"; do
     echo -e "\nFailed: ${#results[@]}\n"
 
     for each in "${results[@]}"; do
-      echo -e "\033[0;31m\u2716\033[0m $each"
-
       if ! grep -Fqx "$each" <<< "$known_fails" && [[ "$each" != "$launch_test" ]]; then
         should_pass+=("$each")
+        echo -e "\033[0;31m\u2716\033[0m $each"
+      else
+        echo "$each"
       fi
     done
   fi
 done
 
-if [[ ${#should_pass[@]} -gt 0 ]]; then
-  echo -e "\n\033[0;31mTests expected to pass but failed: ${#should_pass[@]}\033[0m\n"
+# if [[ ${#should_pass[@]} -gt 0 ]]; then
+#   echo -e "\n\033[0;31mTests expected to pass but failed: ${#should_pass[@]}\033[0m\n"
 
-  for each in "${should_pass[@]}"; do
-    echo -e "$each";
-  done
-fi
+#   for each in "${should_pass[@]}"; do
+#     echo -e "$each";
+#   done
+# fi
 
-if [[ ${#should_not_pass[@]} -gt 0 ]]; then
-  echo -e "\n\033[0;32mTests expected to fail but passed: ${#should_not_pass[@]}\033[0m\n"
+# if [[ ${#should_not_pass[@]} -gt 0 ]]; then
+#   echo -e "\n\033[0;32mTests expected to fail but passed: ${#should_not_pass[@]}\033[0m\n"
 
-  for each in "${should_not_pass[@]}"; do
-    echo -e "$each";
-  done
-fi
+#   for each in "${should_not_pass[@]}"; do
+#     echo -e "$each";
+#   done
+# fi
 
 (( ${#should_not_pass[@]} + ${#should_pass[@]} > 0 )) && exit 1
